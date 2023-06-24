133874e0abcff538849764d9c45f2d29a94336e3
miraaydurgun <miraydurgun67@gmail.com>
2023-06-25 00:34:38 +0300

    Add project files.

---
+++ ApiConsume/HotelProject.BusinessLayer/Concrete/TestimonialManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+
+namespace HotelProject.BusinessLayer.Concrete
+{
+	public class TestimonialManager : ITestimonialService
+	{
+		private readonly ITestimonialDal _testimonialDal;
+
+		public TestimonialManager(ITestimonialDal testimonialDal)
+		{
+			_testimonialDal = testimonialDal;
+		}
+
+		public List<Testimonial> GetList()
+		{
+			return _testimonialDal.GetList();
+		}
+
+		public void TDelete(Testimonial t)
+		{
+			_testimonialDal.Delete(t);
+		}
+
+		public Testimonial TGetByID(int id)
+		{
+			return _testimonialDal.GetByID(id);
+		}
+
+		public void TInsert(Testimonial t)
+		{
+			_testimonialDal.Insert(t);
+		}
+
+		public void TUpdate(Testimonial t)
+		{
+			_testimonialDal.Update(t);
+		}
+	}
+}
---
+++ ApiConsume/HotelProject.DTO/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HotelProject.DTO
+{
+	public class Class1
+	{
+	}
+}
