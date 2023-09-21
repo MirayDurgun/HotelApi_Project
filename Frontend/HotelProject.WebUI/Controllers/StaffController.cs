f999f9bfafc572554af5c8946aecc23bf2e558d5
Miray Durgun <miraydurgun67@gmail.com>
2023-09-21 08:56:53 +0300

    26. Video sonu Güncelleme

--- Frontend/HotelProject.WebUI/Controllers/StaffController.cs
+++ Frontend/HotelProject.WebUI/Controllers/StaffController.cs
@@ -2,6 +2,7 @@
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using HotelProject.WebUI.Models;
 using HotelProject.WebUI.Models.Staff;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
@@ -68,6 +69,22 @@ namespace HotelProject.WebUI.Controllers
             }
             return View();
             //olmazsa viewvi döndürsün
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateStaff(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:2077/api/Staff/{id}");
+            //Güncelleyeceğimiz verileri GetAsync ile önce getiryoruz
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                //veri listeleme yapıyoruz
+                var values = JsonConvert.DeserializeObject<UpdateStaffViewModel>(jsonData);
+                return View(values);
+            }
+            return View();
         }
     }
 }
