f5196261e2fe2157aded38b4596f468725b813b6
Miray Durgun <miraydurgun67@gmail.com>
2023-09-21 08:30:45 +0300

    25. Video sonu personelin silinmesi

--- ApiConsume/HotelProject.WebApi/Controllers/StaffController.cs
+++ ApiConsume/HotelProject.WebApi/Controllers/StaffController.cs
@@ -6,47 +6,51 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HotelProject.WebApi.Controllers
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class StaffController : ControllerBase
-	{
-		private readonly IStaffService _staffService;
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffController : ControllerBase
+    {
+        private readonly IStaffService _staffService;
 
-		public StaffController(IStaffService staffService)
-		{
-			_staffService = staffService;
-		}
+        public StaffController(IStaffService staffService)
+        {
+            _staffService = staffService;
+        }
 
-		[HttpGet] //verileri getirir
-		public IActionResult StaffList()
-		{
-			var values = _staffService.TGetList();
-			return Ok(values);
-		}
-		[HttpPost] //yeni veri ekleme
-		public IActionResult AddStaff(Staff staff)
-		{
-			_staffService.TInsert(staff);
-			return Ok();
-		}
-		[HttpDelete] //verileri silme 
-		public IActionResult DeleteStaff(int id)
-		{
-			var values = _staffService.TGetByID(id);
-			_staffService.TDelete(values);
-			return Ok();
-		}
-		[HttpPut] //verileri günceller
-		public IActionResult UpdateStaff(Staff staff)
-		{
-			_staffService.TUpdate(staff);
-			return Ok();
-		}
-		[HttpGet("{id}")] //dışarıdan bir id parametresi alır
-		public IActionResult GetStaff(int id)
-		{
-			var values = _staffService.TGetByID(id);
-			return Ok(values);
-		}
-	}
+        [HttpGet] //verileri getirir
+        public IActionResult StaffList()
+        {
+            var values = _staffService.TGetList();
+            return Ok(values);
+        }
+
+        [HttpPost] //yeni veri ekleme
+        public IActionResult AddStaff(Staff staff)
+        {
+            _staffService.TInsert(staff);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")] //verileri silme 
+        public IActionResult DeleteStaff(int id)
+        {
+            var values = _staffService.TGetByID(id);
+            _staffService.TDelete(values);
+            return Ok();
+        }
+
+        [HttpPut] //verileri günceller
+        public IActionResult UpdateStaff(Staff staff)
+        {
+            _staffService.TUpdate(staff);
+            return Ok();
+        }
+
+        [HttpGet("{id}")] //dışarıdan bir id parametresi alır
+        public IActionResult GetStaff(int id)
+        {
+            var values = _staffService.TGetByID(id);
+            return Ok(values);
+        }
+    }
 }
--- Frontend/HotelProject.WebUI/Controllers/StaffController.cs
+++ Frontend/HotelProject.WebUI/Controllers/StaffController.cs
@@ -9,49 +9,65 @@ using Newtonsoft.Json;
 
 namespace HotelProject.WebUI.Controllers
 {
-	public class StaffController : Controller
-	//api consume
-	{
-		private readonly IHttpClientFactory _httpClientFactory;
+    //personel controller
+    public class StaffController : Controller
+    //api consume
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
 
-		public StaffController(IHttpClientFactory httpClientFactory)
-		{
-			_httpClientFactory = httpClientFactory;
-		}
+        public StaffController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
 
-		//await kullanılıyorsa metot async olmalı 
-		public async Task<IActionResult> Index()
-		{
-			var client = _httpClientFactory.CreateClient(); //bir tane istemci oluşturduk
-			var responseMessage = await client.GetAsync("http://localhost:2077/api/Staff");
-			//listemelek istediğimizden getasync kullandık
-			if (responseMessage.IsSuccessStatusCode) //başarılı durum kodu dönerse
-			{
-				var jsonData = await responseMessage.Content.ReadAsStringAsync();
-				var values = JsonConvert.DeserializeObject<List<StaffViewModel>>(jsonData);
-				return View(values);
-			}
+        //await kullanılıyorsa metot async olmalı 
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            //bir tane istemci oluşturduk
+            var responseMessage = await client.GetAsync("http://localhost:2077/api/Staff");
+            //listemelek istediğimizden getasync kullandık
+            if (responseMessage.IsSuccessStatusCode)
+            //başarılı durum kodu dönerse
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<StaffViewModel>>(jsonData);
+                return View(values);
+            }
 
-			return View();
-		}
-		[HttpGet]
-		public IActionResult AddStaff()
-		{
-			return View();
-		}
-		[HttpPost]
-		public async Task<IActionResult> AddStaff(AddStaffViewModel model)
-		{
-			var client = _httpClientFactory.CreateClient();
-			var jsonData = JsonConvert.SerializeObject(model);
-			StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-			//Encoding.UTF8 türkçe karakter desteklesin
-			var responsiveMessage = await client.PostAsync("http://localhost:2077/api/Staff", stringContent);
-			if(responsiveMessage.IsSuccessStatusCode)
-			{
-				return RedirectToAction("Index");
-			}
-			return View();
-		}
-	}
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddStaff()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddStaff(AddStaffViewModel model)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            //Encoding.UTF8 türkçe karakter desteklesin
+            var responseMessage = await client.PostAsync("http://localhost:2077/api/Staff", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteStaff(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"http://localhost:2077/api/Staff/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                //Başarılı olursa index'e dönsün 
+                return RedirectToAction("Index");
+            }
+            return View();
+            //olmazsa viewvi döndürsün
+        }
+    }
 }
