db968110bef1d9c37f8a81802429a226f619b153
Miray Durgun <miraydurgun67@gmail.com>
2024-01-30 22:58:35 +0300

    30. Video Sonu

---
+++ ApiConsume/HotelProject.WebApi/Mapping/AutoMapperConfig.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using HotelProject.DtoLayer.Dtos.RoomDto;
+using HotelProject.EntityLayer.Concrete;
+
+namespace HotelProject.WebApi.Mapping
+{
+    public class AutoMapperConfig : Profile
+    //Dto ile entityleri bağlayacağımız sınıf
+
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<RoomAddDto, Room>();
+            // CreateMap yöntemi, iki sınıf arasında bir nesne eşlemesi tanımlar.
+            // RoomAddDto sınıfından Room sınıfına bir eşleme tanımlanıyor.
+
+            CreateMap<Room, RoomAddDto>();
+
+
+            CreateMap<UpdateRoomDto, Room>().ReverseMap();
+            // CreateMap<Room, UpdateRoomDto>();  ReverseMap sayesinde tekrar bu şekilde kod yazmaya gerek kalmaz tersine de mapleme yapar
+
+        }
+    }
+}
