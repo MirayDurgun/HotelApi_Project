081c7bae5bd21c21876a750900a1d95100bbd3fd
Miray Durgun <miraydurgun67@gmail.com>
2024-02-02 19:32:16 +0300

    59. Video sonu

--- Frontend/HotelProject.WebUI/Controllers/BookingController.cs
+++ Frontend/HotelProject.WebUI/Controllers/BookingController.cs
@@ -1,10 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.BookingDto;
+using HotelProject.WebUI.Dtos.SubscribeDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class BookingController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BookingController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
         public IActionResult Index()
         {
             return View();
@@ -17,8 +28,22 @@ namespace HotelProject.WebUI.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> AddBooking(string x)
+        public async Task<IActionResult> AddBooking(CreateBookingDto createBookingDto)
         {
+            createBookingDto.Status = "Onay Bekliyor";
+
+            var client = _httpClientFactory.CreateClient();
+            //bir tane istemci oluşturduk
+            var jsonData = JsonConvert.SerializeObject(createBookingDto);
+            //JSON formatına dönüştür
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            //Encoding.UTF8 türkçe karakter desteklesin
+            var responseMessage = await client.PostAsync("http://localhost:2077/api/Booking", stringContent);
+            //JSON verisi, HTTP isteği için uygun formatta olacak şekilde hazırla
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
