1535acb2cf8d65b7bacc6cd5c29ce43b38f66cd5
Miray Durgun <miraydurgun67@gmail.com>
2024-02-12 21:41:53 +0300

    69. Video sonu

--- RapidApiConsume/Controllers/ImdbController.cs
+++ RapidApiConsume/Controllers/ImdbController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiConsume.Models;
 
 
 namespace RapidApiConsume.Controllers
@@ -7,6 +9,7 @@ namespace RapidApiConsume.Controllers
     {
         public async Task<IActionResult> Index()
         {
+            List<ApiMovieViewModel> apiMovieViewModels = new List<ApiMovieViewModel>();
 
             // HttpClient nesnesi oluşturuluyor.
             var client = new HttpClient();
@@ -14,7 +17,7 @@ namespace RapidApiConsume.Controllers
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Get, // GET metodu kullanılıyor.
-                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/top32"), // API'nin URL'si belirleniyor.
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"), // API'nin URL'si belirleniyor.
                 Headers =
         {
             { "X-RapidAPI-Key", "0d63638b74mshf218c5b70b03499p1ae6bbjsn7318835503c5" }, // API isteği için kullanılacak olan anahtar (key).
@@ -28,8 +31,10 @@ namespace RapidApiConsume.Controllers
                 response.EnsureSuccessStatusCode();
                 // Cevap içeriği okunuyor ve bir değişkene atanıyor.
                 var body = await response.Content.ReadAsStringAsync();
+
+                apiMovieViewModels = JsonConvert.DeserializeObject<List<ApiMovieViewModel>>(body);
+                return View(apiMovieViewModels);
             }
-            return View();
         }
     }
 }
