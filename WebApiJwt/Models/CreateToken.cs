024b66da5ac10b3896cd2b28af7f13f4bfd0be36
Miray Durgun <miraydurgun67@gmail.com>
2024-02-14 16:32:54 +0300

    79. Video sonu

---
+++ WebApiJwt/Models/CreateToken.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace WebApiJwt.Models
+{
+    public class CreateToken
+    {
+        public void TokenCreate()
+        {
+            // Anahtar olarak kullanılacak string değerini UTF-8 formatına dönüştürür
+            var bytes = Encoding.UTF8.GetBytes("aspnetcoreapiapi");
+
+            // JWT'nin imzalanması için kullanılacak simetrik anahtarı oluşturur
+            SymmetricSecurityKey key = new SymmetricSecurityKey(bytes);
+
+            // İmzalama yetkilendirmelerini sağlayan bir nesne oluşturur
+            SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            // JWT oluşturur
+            JwtSecurityToken token = new JwtSecurityToken(
+                // JWT'nin düzenleyicisini belirtir
+                issuer: "http://localhost",
+                // JWT'nin hedef kitlesini belirtir
+                audience: "http://localhost",
+                // Token'in ne zaman kullanılabileceğini belirtir
+                notBefore: DateTime.Now,
+                // Token'in ne zaman geçersiz hale geleceğini belirtir
+                expires: DateTime.Now.AddMinutes(3),
+                // JWT'nin imzalanması için kullanılacak kimlik bilgilerini sağlar
+                signingCredentials: credentials
+            );
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            handler.WriteToken(token);
+
+        }
+    }
+}
--- WebApiJwt/Program.cs
+++ WebApiJwt/Program.cs
@@ -10,17 +10,37 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+// Kimlik doðrulama iþlemlerini yapýlandýrýr
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
 {
+    // HTTPS kullanýmýný zorunlu kýlmaz
     opt.RequireHttpsMetadata = false;
+
+    // Token doðrulama parametrelerini ayarlar
     opt.TokenValidationParameters = new TokenValidationParameters()
     {
+        // Geçerli Issuer'ý belirtir. JWT'yi düzenleyen veya oluþturan tarafý temsil eder. Yani, tokenin hangi kaynaktan geldiðini belirtir.kd
         ValidIssuer = "http://localhost",
+
+        // Geçerli Audience'ý belirtir. JWT'nin hedef kitlesini temsil eder. Yani, tokenin hangi kaynak veya servise hitap ettiðini belirtir.
         ValidAudience = "http://localhost",
+
+        // Issuer'ý doðrulamak için kullanýlacak anahtarý belirtir
         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("aspnetcoreapiapi")),
-        ValidateIssuerSigningKey = true
+
+        // Issuer'ýn imzalama anahtarýný doðrulamasýný saðlar
+        ValidateIssuerSigningKey = true,
+
+        // Token'ýn süresini doðrular
+        ValidateLifetime = true
     };
 });
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
