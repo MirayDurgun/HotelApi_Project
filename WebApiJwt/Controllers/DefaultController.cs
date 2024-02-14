bf4e08e4f49ea55fb933b44ec900c043c598248f
Miray Durgun <miraydurgun67@gmail.com>
2024-02-14 18:24:09 +0300

    83. Video sonu

--- WebApiJwt/Controllers/DefaultController.cs
+++ WebApiJwt/Controllers/DefaultController.cs
@@ -15,11 +15,24 @@ namespace WebApiJwt.Controllers
             return Ok(new CreateToken().TokenCreate());
         }
 
+        [HttpGet("[action]")]
+        public IActionResult Test()
+        {
+            return Ok(new CreateToken().TokenCreate());
+        }
+
         [Authorize]
         [HttpGet("[action]")]
         public IActionResult Test2()
         {
             return Ok("Hoş Geldiniz");
+        }
+
+        [Authorize(Roles = "Admin,Visitor")]
+        [HttpGet("[action]")]
+        public IActionResult Test3()
+        {
+            return Ok("Token Başarılı Bir Şekilde Giriş Yaptı");
         }
     }
 }
