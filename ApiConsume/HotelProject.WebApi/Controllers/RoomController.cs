bdf8156552b013bbee8f37b94dd2622d746f5fce
Miray Durgun <miraydurgun67@gmail.com>
2024-02-20 21:42:29 +0300

    95. Video sonu

--- ApiConsume/HotelProject.WebApi/Controllers/RoomController.cs
+++ ApiConsume/HotelProject.WebApi/Controllers/RoomController.cs
@@ -6,47 +6,47 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HotelProject.WebApi.Controllers
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class RoomController : ControllerBase
-	{
-		private readonly IRoomService _roomService;
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomController : ControllerBase
+    {
+        private readonly IRoomService _roomService;
 
-		public RoomController(IRoomService roomService)
-		{
-			_roomService = roomService;
-		}
+        public RoomController(IRoomService roomService)
+        {
+            _roomService = roomService;
+        }
 
-		[HttpGet] //verileri getirir
-		public IActionResult RoomList()
-		{
-			var values = _roomService.TGetList();
-			return Ok(values);
-		}
-		[HttpPost] //yeni veri ekleme
-		public IActionResult AddRoom(Room room)
-		{
-			_roomService.TInsert(room);
-			return Ok();
-		}
-		[HttpDelete] //verileri silme 
-		public IActionResult DeleteRoom(int id)
-		{
-			var values = _roomService.TGetByID(id);
-			_roomService.TDelete(values);
-			return Ok();
-		}
-		[HttpPut] //verileri günceller
-		public IActionResult UpdateRoom(Room room)
-		{
-			_roomService.TUpdate(room);
-			return Ok();
-		}
-		[HttpGet("{id}")] //dışarıdan bir id parametresi alır
-		public IActionResult GetRoom(int id)
-		{
-			var values = _roomService.TGetByID(id);
-			return Ok(values);
-		}
-	}
+        [HttpGet] //verileri getirir
+        public IActionResult RoomList()
+        {
+            var values = _roomService.TGetList();
+            return Ok(values);
+        }
+        [HttpPost] //yeni veri ekleme
+        public IActionResult AddRoom(Room room)
+        {
+            _roomService.TInsert(room);
+            return Ok();
+        }
+        [HttpDelete("{id}")] //verileri silme 
+        public IActionResult DeleteRoom(int id)
+        {
+            var values = _roomService.TGetByID(id);
+            _roomService.TDelete(values);
+            return Ok();
+        }
+        [HttpPut] //verileri günceller
+        public IActionResult UpdateRoom(Room room)
+        {
+            _roomService.TUpdate(room);
+            return Ok();
+        }
+        [HttpGet("{id}")] //dışarıdan bir id parametresi alır
+        public IActionResult GetRoom(int id)
+        {
+            var values = _roomService.TGetByID(id);
+            return Ok(values);
+        }
+    }
 }
