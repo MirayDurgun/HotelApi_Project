2f7e9eae83ec5acaf921173392258c0faa903188
Miray Durgun <miraydurgun67@gmail.com>
2024-02-20 23:14:29 +0300

    96. Video sonu

---
+++ ApiConsume/HotelProject.WebApi/Controllers/GuestController.cs
@@ -0,0 +1,51 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GuestController : ControllerBase
+    {
+        private readonly IGuestService _guestService;
+
+        public GuestController(IGuestService guestService)
+        {
+            _guestService = guestService;
+        }
+
+        [HttpGet] //verileri getirir
+        public IActionResult GuestList()
+        {
+            var values = _guestService.TGetList();
+            return Ok(values);
+        }
+        [HttpPost] //yeni veri ekleme
+        public IActionResult AddGuest(Guest guest)
+        {
+            _guestService.TInsert(guest);
+            return Ok();
+        }
+        [HttpDelete("{id}")] //verileri silme 
+        public IActionResult DeleteGuest(int id)
+        {
+            var values = _guestService.TGetByID(id);
+            _guestService.TDelete(values);
+            return Ok();
+        }
+        [HttpPut] //verileri günceller
+        public IActionResult UpdateGuest(Guest guest)
+        {
+            _guestService.TUpdate(guest);
+            return Ok();
+        }
+        [HttpGet("{id}")] //dışarıdan bir id parametresi alır
+        public IActionResult GetGuest(int id)
+        {
+            var values = _guestService.TGetByID(id);
+            return Ok(values);
+        }
+    }
+}
--- ApiConsume/HotelProject.WebApi/Startup.cs
+++ ApiConsume/HotelProject.WebApi/Startup.cs
@@ -56,6 +56,8 @@ namespace HotelProject.WebApi
             services.AddScoped<IContactDal, EfContactDal>();
             services.AddScoped<IContactService, ContactManager>();
 
+            services.AddScoped<IGuestDal, EfGuestDal>();
+            services.AddScoped<IGuestService, GuestManager>();
 
 
             services.AddAutoMapper(typeof(Startup));
